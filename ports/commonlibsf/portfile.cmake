6bbf6f820d4573e343e9f659acc9a5b2b20022ce
Angad <angadmisra28@gmail.com>
2023-09-07 16:46:35 -0700

    Initial registry setup

--- ports/commonlibsf/portfile.cmake
+++ ports/commonlibsf/portfile.cmake
@@ -1,31 +1,26 @@
-# FIXME: copied from CommonLibSSE-NG portfile
-
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO Starfield-Reverse-Engineering/CommonLibSF
-  REF TODO
-  SHA512 TODO
-  HEAD_REF main
-)
+  REF beccc06cc40cee28c304183b2676ff00e4278aa4
+  SHA512 6b2616a3d5fc1493b70c54d1edea5609f46318939084ffe1f9f0cedad2acf1077ee6721ac841b53db154749931b43f24213cc8b1cc8b65c3a9e7bdcc4e0de11b
+  HEAD_REF main)
 
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-vcpkg_configure_cmake(
-        SOURCE_PATH "${SOURCE_PATH}"
-        PREFER_NINJA
-)
+vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}")
 
 vcpkg_install_cmake()
 vcpkg_cmake_config_fixup(PACKAGE_NAME CommonLibSF CONFIG_PATH lib/cmake)
 vcpkg_copy_pdbs()
 
-file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/CommonLibSF/CommonLibSSE/*.cmake")
+file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/CommonLibSF/CommonLibSF/*.cmake")
 file(INSTALL ${CMAKE_CONFIGS} DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSF")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/CommonLibSF/CommonLibSF")
 
 file(
-        INSTALL "${SOURCE_PATH}/LICENSE"
+        INSTALL "${SOURCE_PATH}/COPYING"
         DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
         RENAME copyright)
