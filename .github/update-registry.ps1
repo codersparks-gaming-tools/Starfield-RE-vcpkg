b1d93ef81911216e8e9e2902cca03c4ce2954832
DK <gottyduke@hotmail.com>
2023-09-08 11:07:29 -0500

    feat: update-registry ci (#1)

---
+++ .github/update-registry.ps1
@@ -0,0 +1,207 @@
+#Requires -Version 5
+
+# args
+param (
+    [string]$ShaIn = 0
+)
+
+function Out-Json {
+    param(
+        [string]$outfile,
+        [Object]$json
+    )
+
+    $json = $json | ConvertTo-Json -Depth 9 | ForEach-Object { $_ -replace "(?m)  (?<=^(?:  )*)", "  " }
+    [IO.File]::WriteAllText($outfile, $json)
+}
+
+function Get-Latest {
+    param(
+        [string]$repo
+    )
+
+    $parse = (& git ls-remote "$repo.git" rev-parse HEAD) -split '\s'
+    return $parse[0]
+}
+
+
+try {
+    $repo = Resolve-Path $PSScriptRoot/../
+    Push-Location $repo
+
+    # test-path
+    $manifests = @(
+        "$PSScriptRoot/portfile.cmake.in",
+        "$repo/ports/commonlibsf/portfile.cmake",
+        "$repo/ports/commonlibsf/vcpkg.json",
+        "$repo/versions/c-/commonlibsf.json",
+        "$repo/versions/baseline.json",
+        "$repo/README.md"
+    )
+    
+    Write-Host "Checking files..."
+    foreach ($file in $manifests) {
+        if (!(Test-Path $file)) {
+            throw "File not found : {$file}"
+        }
+    }
+    Write-Host "...Ok, all files present"
+    
+    
+    # check ShaIn
+    Write-Host "Checking REF..."
+    if ($ShaIn -eq 0) {
+        # use latest, this is meant for manual workflow dispatch
+        $ShaIn = Get-Latest "https://github.com/$($env:VCPKG_PORT_REPO)/CommonLibSF"
+    }
+
+    if ($ShaIn -eq 0) {
+        throw "Invalid REF for main repo"
+    }
+    Write-Host "...Upstream: $($env:VCPKG_PORT_REPO)`nREF: $($ShaIn)"
+    
+    
+    # calc hash
+    Write-Host "Hashing tarball..."
+    $uri = "https://github.com/$($env:VCPKG_PORT_REPO)/CommonLibSF/archive/$ShaIn.tar.gz"
+    Invoke-WebRequest -Uri $uri -OutFile "$PSScriptRoot/tarball"
+    $hash = (Get-FileHash "$PSScriptRoot/tarball" -Algorithm SHA512).Hash
+    Write-Host "...SHA512 $($hash)"
+
+
+    # make portfile
+    Write-Host "Making portfile..."
+    $header = "
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO $($env:VCPKG_PORT_REPO)/CommonLibSF
+"
+    $portfile = [System.Collections.ArrayList]::new()
+    $portfile.Add($header) | Out-Null
+    $portfile.Add(" REF $ShaIn`n") | Out-Null
+    $portfile.Add(" SHA512 $hash`n") | Out-Null
+    $portfile.Add(" HEAD_REF main)`n") | Out-Null
+    $portfile.Add([IO.File]::ReadAllText($manifests[0])) | Out-Null
+    
+    [IO.File]::WriteAllText($manifests[1], $portfile)
+   
+    Write-Host "...Ok"
+    
+
+    # fetch vcpkg.json
+    Write-Host "Fetching vcpkg.json from upstream..."
+    $uri = "https://raw.githubusercontent.com/$($env:VCPKG_PORT_REPO)/CommonLibSF/$ShaIn/CommonLibSF/vcpkg.json"
+    $vcpkg = Invoke-RestMethod -Uri $uri
+    $version = $vcpkg.'version-date'
+    
+    # make vcpkg.json
+    Out-Json $manifests[2] $vcpkg
+    Write-Host "...Ok"
+    
+
+    # test build
+    Write-Host "Testing port package..."
+    $triplet = "x64-windows-static"
+    $failed = $false
+    $vcpkg_test = & "$($env:VCPKG_ROOT)/vcpkg" install commonlibsf:$triplet --overlay-ports="$repo/ports/commonlibsf" | ForEach-Object {
+        if ($_.StartsWith("error: ")) {
+            $script:failed = $true
+        }
+        $_ + "`n"
+    }
+    if ($failed) {
+        throw "Cannot compile vcpkg port:`n$vcpkg_test"
+    }
+    Write-Host "...Ok"
+
+
+    # update baseline
+    Write-Host "Updating baseline..."
+    $baseline = [IO.File]::ReadAllText($manifests[4]) | ConvertFrom-Json
+    $baseline.default.commonlibsf.baseline = $version
+    Out-Json $manifests[4] $baseline
+    Write-Host "New baseline -> $version"
+
+
+    # update version entries
+    Write-Host "Propagating version entry..."
+    $gitlog = & git config user.name "vcpkg-action"
+    $gitlog += & git config user.email "gottyduke@hotmail.com"
+    $gitlog += & git add $manifests
+    $gitlog += & git commit -m "port: check"
+    $gitlog += & git rev-parse HEAD:ports/commonlibsf
+    $tree = & git rev-parse HEAD:ports/commonlibsf
+
+    $entries = [IO.File]::ReadAllText($manifests[3]) | ConvertFrom-Json
+    # check for collision
+    $collision = $false
+    foreach ($entry in $entries.versions) {
+        if ($entry.version -eq $version) {
+            Write-Host "Updating existing entry $version"
+            $entry.'git-tree' = $tree
+            $collision = $true
+            break
+        }
+    }
+    if (!$collision) {
+        Write-Host "Adding new entry $version"
+        $entry = '{ "version": "", "port-version": 0, "git-tree": "" }' | ConvertFrom-Json
+        $entry.version = $version
+        $entry.'git-tree' = $tree
+        $entries.versions += $entry
+    }
+    Out-Json $manifests[3] $entries
+    Write-Host "...Ok, git-tree set to $tree"
+
+
+    # commit
+    Write-Host "Committing vcpkg port..."
+    $gitlog += & git commit --amend -m "port: ``$version``"
+
+
+    # update docs
+    Write-Host "Updating docs..."
+    $readme = [IO.File]::ReadAllLines($manifests[5])
+    $vcpkg_latest = Get-Latest "https://github.com/microsoft/vcpkg"
+    $port_latest = & git rev-parse HEAD
+    $readme = $readme -replace "(?<=label=vcpkg%20registry&message=).+?(?=&color)", $version
+    for ($i = 0; $i -lt $readme.Length; $i = $i + 1) {
+        if ($readme[$i].Contains("https://github.com/microsoft/vcpkg")) {
+            $readme[$i + 1] = $readme[$i + 1] -replace '(?<="baseline": ").*?(?=")', $vcpkg_latest
+            $i = $i + 1
+            continue
+        }
+        if ($readme[$i].Contains("https://github.com/Starfield-Reverse-Engineering/Starfield-RE-vcpkg")) {
+            $readme[$i + 1] = $readme[$i + 1] -replace '(?<="baseline": ").*?(?=")', $port_latest
+            $i = $i + 1
+            continue
+        }
+    }
+    [IO.File]::WriteAllLines($manifests[5], $readme)
+    Write-Host "...Ok"
+
+
+    # check gitlog
+    $failed = $false
+    foreach ($log in $gitlog) {
+        if ($log.StartsWith("fatal: ")) {
+            $failed = $true
+            break
+        }
+    }
+    if ($failed) {
+        throw "Cannot commit action:`n$gitlog"
+    }
+
+    Write-Output "::set-output name=VCPKG_SUCCESS::true"
+}
+catch {
+    Write-Error "...Failed: $_"
+    Write-Output "::set-output name=VCPKG_SUCCESS::false"
+}
+finally {
+    Pop-Location
+    Remove-Item "$PSScriptRoot/tarball" -Force -Confirm:$false -ErrorAction:SilentlyContinue | Out-Null
+    exit
+}
+
--- README.md
+++ README.md
@@ -2,28 +2,55 @@
 
 ## CommonLibSF
 
+[![VCPKG_VER](https://img.shields.io/static/v1?label=vcpkg%20registry&message=2023-09-08&color=green&style=flat)](https://github.com/Starfield-Reverse-Engineering/Starfield-RE-vcpkg)
+
 ### Including CommonLibSF in your project
 
-Add the following to your `vcpkg-configuration`:
+#### Using the example plugin
+( TODO )
+
+#### Manual
+CommonLibSF is available as a vcpkg port. To add it to your project, 
+
+1. Create a `vcpkg-configuration.json` file in the project root (next to `vcpkg.json`) OR append the existing `vcpkg.json` with the following contents:
 
 ```json
-"registries": [
-  {
-    "kind": "git",
-    "repository": "https://github.com/Starfield-Reverse-Engineering/Starfield-RE-vcpkg",
-    "baseline": "<SHA of latest commit to the above repository>",
-    "packages": [
-      "commonlibsf"
+{
+    "default-registry": {
+        "kind": "git",
+        "repository": "https://github.com/microsoft/vcpkg",
+        "baseline": "<SHA>"
+    },
+    "registries": [
+        {
+            "kind": "git",
+            "repository": "https://github.com/Starfield-Reverse-Engineering/Starfield-RE-vcpkg",
+            "baseline": "<SHA>",
+            "packages": [
+                "commonlibsf"
+            ]
+        }
     ]
-  }
-]
+}
 ```
 
-And the following to your `CMakeLists.txt`:
+2. Install the package through `vcpkg.json`, append it to your existing dependencies:
+
+```json
+{
+    "dependencies": [
+        "commonlibsf"
+    ]
+}
+```
+
+3. Link it in your `CMakeLists.txt`:
 
 ```cmake
 find_package(CommonLibSF CONFIG REQUIRED)
-target_link_libraries(${PROJECT_NAME}
-        PRIVATE
-        CommonLibSF::CommonLibSF)
+target_link_libraries(
+    ${PROJECT_NAME}
+    PRIVATE
+    CommonLibSF::CommonLibSF
+)
 ```
